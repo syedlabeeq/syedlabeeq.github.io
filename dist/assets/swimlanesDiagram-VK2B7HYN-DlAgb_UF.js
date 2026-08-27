8f7c853af8a9a61f54e7b228cd43e367b860814b
syedlabeeq <syedlabeeq@gmail.com>
2026-08-27 17:29:16 +0500

    Prepare site for Github Pages

--- dist/assets/swimlanesDiagram-VK2B7HYN-DlAgb_UF.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-A5DVABFB-CU2vK9DD.js";import{_ as a}from"./mermaid.core-bcmr_iMF.js";import"./chunk-5VM5RSS4-BS6wiEuj.js";import"./chunk-XXDRQBXY-D2GseS7i.js";import"./chunk-POPQ4Y6H-yuYh46MJ.js";import"./chunk-F27PBJKO-CiQr55kT.js";import"./channel-DNW3FFef.js";import"./index-DzabJYrr.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
